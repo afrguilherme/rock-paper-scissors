1db23de564a2d3253b2275785253b848e762a2e3
afrguilherme <augustogafr@gmail.com>
2023-05-31 20:49:22 -0300

    script optimization

--- scripts.js
+++ scripts.js
@@ -1,56 +1,49 @@
-const result = document.getElementById("result");
-const myScore = document.getElementById("my-score");
-const machineScore = document.getElementById("machine-score");
-const resetButton = document.getElementsByClassName("reset-button");
-const machineChoice = document.getElementById("machine-choice");
+const result = document.querySelector("#result");
+const myScore = document.querySelector("#my-score");
+const machineScore = document.querySelector("#machine-score");
+const resetButton = document.querySelector(".reset-button");
 
-let myScoreNumber = 0;
-let machineScoreNumber = 0;
+let myScoreCounter = 0;
+let machineScoreCounter = 0;
 
-const playHuman = (humanChoice) => {
-  playTheGame(humanChoice, playMachine());
+const myPlay = (playerChoice) => {
+  const machine = machinePlay();
+  console.log("Minha jogada: " + playerChoice);
+  console.log("Jogada da máquina: " + machine);
+  gameRules(playerChoice, machine);
 };
 
-const playMachine = () => {
-  const choices = ["rock", "paper", "scissors"];
-  const randomNumber = Math.floor(Math.random() * 3);
-
-  return choices[randomNumber];
+const machinePlay = () => {
+  const choice = ["rock", "paper", "scissors"];
+  const machineChoice = Math.floor(Math.random() * 3);
+  return choice[machineChoice];
 };
 
-const playTheGame = (human, machine) => {
-  const machineChoice = document.getElementById("machine-choice");
-  if (machine === "rock") {
-    machineChoice.innerHTML = "Alexa chose rock!";
-  } else if (machine === "paper") {
-    machineChoice.innerHTML = "Alexa chose paper!";
-  } else {
-    machineChoice.innerHTML = "Alexa chose scissors!";
-  }
+const gameRules = (player, machine) => {
+  const machineChoice = document.querySelector("#machine-choice");
+  machineChoice.innerHTML = `Alexa chose ${machine}!`;
 
-  if (human === machine) {
+  if (player === machine) {
     result.innerHTML = "It's a draw game!";
   } else if (
-    (human === "rock" && machine === "scissors") ||
-    (human === "scissors" && machine === "paper") ||
-    (human === "paper" && machine === "rock")
+    (player === "rock" && machine === "scissors") ||
+    (player === "scissors" && machine === "paper") ||
+    (player === "paper" && machine === "rock")
   ) {
     result.innerHTML = "Winner!";
-
-    myScoreNumber++;
-    myScore.innerHTML = myScoreNumber;
+    myScoreCounter++;
+    myScore.innerHTML = myScoreCounter;
   } else {
-    result.innerHTML = "You lost!";
-    machineScoreNumber++;
-    machineScore.innerHTML = machineScoreNumber;
+    result.innerHTML = "You Lost!";
+    machineScoreCounter++;
+    machineScore.innerHTML = machineScoreCounter;
   }
 };
 
 const resetScore = () => {
-  myScore.innerHTML = "0";
-  machineScore.innerHTML = "0";
   result.innerHTML = "";
-  machineChoice.innerHTML = "";
-  myScoreNumber = 0;
-  machineScoreNumber = 0;
+  machineScore.innerHTML = "0";
+  myScore.innerHTML = "0";
+  myScoreCounter = 0;
+  machineScoreCounter = 0;
 };
